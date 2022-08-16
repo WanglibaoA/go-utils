f70ff326bb2c5e230d0c2c1c2d5030cb2cfd907b
v_libaowang <v_libaowang@tencent.com>
2022-08-16 16:12:30 +0800

    init,代码首次提交

---
+++ model/constant.go
@@ -0,0 +1,10 @@
+package model
+
+const (
+	YearMonthDay             = "2006/01/02"
+	DefaultTimeFormatPattern = "2006-01-02 15:04:05"
+	DefaultTimeFormatMinute  = "2006-01-02 15:04"
+	DefaultTimeFormatDay     = "2006-01-02"
+	DefaultTimeFormatTime    = "01-02 15:04"
+	DefaultTimeUTC           = "2006-01-02T15:04:05.000+08:00"
+)
---
+++ utils/common_result.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+const (
+	SUCCESS int = 0 // 操作成功
+	FAILED  int = 1
+)
+
+func GinSuccess(ctx *gin.Context, v interface{}) {
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"code": SUCCESS,
+		"msg":  "成功",
+		"data": v,
+	})
+}
+
+func GinFailed(ctx *gin.Context, v interface{}) {
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"code": FAILED,
+		"msg":  v,
+	})
+}
